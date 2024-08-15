cb2fabe37b7a78535ff8e9fac7bf8636e38e3a14
banushasarma <sharmabanu03@gmail.com>
2024-08-15 20:33:19 +0530

    Added admin module

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -3,7 +3,9 @@ import fs from "fs";
 
 //add food item
 const addFood = async (req, res) => {
+
     let image_filename = `${req.file.filename}`;
+
     const food = new foodModel({
         name: req.body.name,
         description: req.body.description,
@@ -21,4 +23,30 @@ const addFood = async (req, res) => {
     }
 }
 
-export { addFood };
+//All food list 
+const listFood = async (req, res) => {
+    try {
+        const foods = await foodModel.find({});
+        res.json({ success: true, data: foods });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+}
+
+//Remove food
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findByIdAndDelete(req.body.id);
+
+        if (!food) return res.status(404).json({ success: false, message: "Food not found" });
+
+        fs.unlinkSync(`uploads/${food.image}`);
+        res.json({ success: true, message: "Food Removed" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+}
+
+export { addFood, listFood, removeFood };
--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addFood } from '../controllers/foodController.js'
+import { addFood, listFood, removeFood } from '../controllers/foodController.js'
 import multer from 'multer';
 
 const foodRouter = express.Router();
@@ -15,6 +15,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
+foodRouter.delete("/remove", removeFood);
 
 
 
